d6e52f1e47f2bceb2b266a425c1828fcb03076e7
xplicitone <paul.kalaw@gmail.com>
2023-01-18 23:07:38 -0600

    Add Carousel as first Class component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
@@ -40,6 +41,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
