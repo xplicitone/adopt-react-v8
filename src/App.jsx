fa4e054e3f258228f88d998aaa18ecb2f7e680ac
xplicitone <paul.kalaw@gmail.com>
2023-01-26 14:37:26 -0600

    first code-splitting example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
+
+// if something tries to render details, panic and load details.
+// Vite smart enough to know it doesnt need it on initial page load so wont load it right away
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 // Infinity meaning once you fetch something, do not refetch it for the whole "session"
 const queryClient = new QueryClient({
@@ -21,15 +24,23 @@ const App = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </AdoptedPetContext.Provider>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🦔</h2>
+            </div>
+          }
+        >
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </Suspense>
       </QueryClientProvider>
     </BrowserRouter>
   );
