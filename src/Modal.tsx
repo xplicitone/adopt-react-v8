11fc16bdc1cc3627c17f6d49ebdefc23ce844212
xplicitone <paul.kalaw@gmail.com>
2023-01-28 11:35:04 -0600

    convert modal to ts and add ts eslint

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
 // pass children through if it has otherwise it wont render itself
-const Modal = ({ children }) => {
+const Modal = ({ children }: { children: ReactElement }) => {
   // ref is i have this piece of something i want to have the same thing back everytime
   // ref is container to give yourself back same thing every time.
-  const elRef = useRef(null);
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   // if don't have a ref, create one. want same div on every single re-render.
   if (!elRef.current) {
@@ -14,6 +14,9 @@ const Modal = ({ children }) => {
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
     // need cleanup. Class you would use ComponentWillUnmount bc component about to exit the DOM you wanna run that method. For FUnction component:
 
@@ -21,7 +24,11 @@ const Modal = ({ children }) => {
     // remove child at end otherwise infinite divs left inside of this portal/modal.
     // if doing some external event listening, go in here and remove the event listener
     // stopping a timer (setTimeout or requestAnimationFrame or setInterval), cancel it here.
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   // can say (children) but for css classes / styling, it needs it
